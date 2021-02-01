03929991bf4b106c105ad329f1c830136661ee55
sebastian <sebastian bowen>
2021-02-01 15:41:41 -0700

    functions and interactions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! Javascript is running!");
+//this creates a function named "fight"
+var playerName = window.prompt("what is your robots name?");
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10+10);
+// what is this
+console.log("our robots name is" + playerName);
+
+function fight() {
+    window.alert("the fight has begun");
+}// fight();
