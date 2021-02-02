1b3e25f20eb43ed1fb437b6356a44b5b10b39966
sebastian <sebastian bowen>
2021-02-01 18:09:29 -0700

    robots can fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,43 @@
-//this creates a function named "fight"
-var playerName = window.prompt("what is your robots name?");
-console.log(playerName);
-console.log("this logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10+10);
-// what is this
-console.log("our robots name is" + playerName);
+var playerName = window.prompt("whatis your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-    window.alert("the fight has begun");
-}// fight();
+var enemyName = "roberto";
+var enemyHealth = 50;
+var enemyAttack= 12;
+
+var fight = function() {
+    //Alert players that they are stsrting a round
+    window.alert("Welcome to robot gladiators!");
+
+    //Subtract the value of 'playerAttack from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+  enemyHealth = enemyHealth - playerAttack;
+    //Log a resulting message to the console so we know it worked
+console.log(
+    playerName + "attacked" +  enemyName + "." +enemyName + "now has" + enemyHealth + "health remaining."
+);
+
+//check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + "has died!");
+}
+else {
+    window.alert(enemyName + "still has" + enemyHealth + "health left.");
+}
+    //subtract the value of 'enemyAttack' from the value of 'playerHealth'  and use that result to update the value in the 'playerHealth' variable.
+playerHealth = playerHealth - enemyAttack;
+    // log a resulting message to the console so we know it worked.
+    console.log(
+    enemyName + "attacked" + playerName + "."  + playerName +"now has" + playerHealth + "health remaining."
+    );
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + "has died!");
+    }
+    else {
+        window.alert(playerName + "still has" + playerHealth + "health left");
+    }
+    };
+
+
+fight()
